ff2750de544b8f423c713a9e116a531ae7ab7f66
kota0120 <09041150239@docomo.ne.jp>
2026-07-21 06:26:15 +0900

    ユーザの登録用のコントローラを作成

--- src/main/java/com/example/demo/user/controller/RegisterController.java
+++ src/main/java/com/example/demo/user/controller/RegisterController.java
@@ -1,7 +1,11 @@
 package com.example.demo.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.user.service.UserRegisterService;
 
@@ -14,7 +18,29 @@ private final UserRegisterService userRegisterService;
         this.userRegisterService = userRegisterService;
     }
 
-    
+    // 登録フォームの表示
+    @GetMapping("/register")
+    public String showRegisterForm(){
+        return "register";
+    }
+
+    // 登録処理
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String email, @RequestParam String password, @RequestParam String confirmPassword, Model model){
+
+        // 確認用パスワードとの一致を確認
+        if(!password.equals(confirmPassword)){
+            model.addAttribute("registerError", "登録したパスワードと一致しません。");
+            model.addAttribute("email", email);
+            return "register";
+        }
+
+        // ユーザーを登録
+
+
+
+        return "register";
+    }
 
 
 
